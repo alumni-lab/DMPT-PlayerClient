2f3d337b52a350df9e74d23b16dddd1f0ed6d529
Haibin Zhu <ssspiderzzz@gmail.com>
2020-01-26 16:56:02 -0800

    get message form server

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import Cookies from 'js-cookie'
+const baseURL = 'https://c27ee1b8.ngrok.io/'
 
-const Login = () => {
+const Login = props => {
   const onChange = event => {
     console.log(event.target.value)
   }
 
-  const onSubmit = () => {
-    axios.post('c27ee1b8.ngrok.io/api/connectionStatus').then(res => {
-      console.log(res)
-    })
-  }
+  const [state, setState] = useState({
+    new_user: false,
+    errorMessage: false,
+    serverOnline: false,
+    screenWidth: ''
+  })
+
+  useEffect(() => {
+    if (!Cookies.get('token')) {
+      axios({
+        method: 'get',
+        url: `${baseURL}api/connectionStatus`
+      })
+        .then(response => {
+          if (response.data.message === 'ONLINE')
+            setState({
+              serverOnline: true
+            })
+          console.log(`test Online`)
+        })
+        .catch(function (e) {
+          console.log(e)
+        })
+    }
+  }, [])
 
   return (
     <>
@@ -19,7 +41,7 @@ const Login = () => {
       <input></input>
       <span>Password</span>
       <input onChange={event => onChange(event)}></input>
-      <button onClick={onSubmit}>Submit</button>
+      <button>Submit</button>
     </>
   )
 }
