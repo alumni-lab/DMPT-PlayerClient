1bd9ff926c934bd574ffaca259469e5bd9694bc2
Haibin Zhu <ssspiderzzz@gmail.com>
2020-01-26 16:32:43 -0800

    install axios, test to reach server

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,14 +1,25 @@
 import React from 'react'
+import axios from 'axios'
 
 const Login = () => {
+  const onChange = event => {
+    console.log(event.target.value)
+  }
+
+  const onSubmit = () => {
+    axios.post('c27ee1b8.ngrok.io/api/connectionStatus').then(res => {
+      console.log(res)
+    })
+  }
+
   return (
     <>
       <div>Log in</div>
-      <div>Email</div>
+      <span>Email</span>
       <input></input>
-      <div>Password</div>
-      <input></input>
-      <button>Submit</button>
+      <span>Password</span>
+      <input onChange={event => onChange(event)}></input>
+      <button onClick={onSubmit}>Submit</button>
     </>
   )
 }
