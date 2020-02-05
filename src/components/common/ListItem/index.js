8e3ca87e1aad06adecc6c878e720c0610fd20727
Steven Wing <steven.wing82@gmail.com>
2020-02-04 18:17:34 -0800

    add second theme for testing

--- src/components/common/ListItem/index.js
+++ src/components/common/ListItem/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import {fonts, colours} from '../styles/index.js';
 
+const colour = colours.base; // set theme
 
 const Item = styled.div`
   display: flex;
@@ -9,12 +10,12 @@ const Item = styled.div`
   width: 100%;
   min-width: 220px;
   max-width: 20vw;
-  box-shadow: 0px 0px 0px 1px ${colours.backgroundDark};
+  box-shadow: 0px 0px 0px 1px ${colour.backgroundDark};
   margin: 0;
   font-family: ${fonts.main.family};
   transition: all 2s ease;
   user-select: none; 
-  background-color: ${colours.background}
+  background-color: ${colour.background}
   `
 const Title = styled.p`
   text-align: center;
@@ -23,21 +24,21 @@ const Title = styled.p`
   width: 100%;
   cursor: pointer;
   // ${({ expanded }) => expanded && 'border-bottom: 1px solid black;'}
-  color: ${colours.primary};
-  text-shadow: 1px 1px 1px ${colours.primaryShadow};
+  color: ${colour.primary};
+  text-shadow: 1px 1px 1px ${colour.primaryShadow};
   line-height: 33px;
   font-size: ${fonts.main.size.medium};
-  background: ${colours.backgroundDark};
-  background: -moz-linear-gradient(top, ${colours.backgroundDark} 1%, ${colours.background} 100%);
-  background: -webkit-gradient(linear, left top, left bottom, color-stop(1%,${colours.backgroundDark}), color-stop(100%,${colours.background}));
-  background: -webkit-linear-gradient(top, ${colours.backgroundDark} 1%,${colours.background} 100%);
-  background: -o-linear-gradient(top, ${colours.backgroundDark} 1%,${colours.background} 100%);
-  background: -ms-linear-gradient(top, ${colours.backgroundDark} 1%,${colours.background} 100%);
-  background: linear-gradient(top, ${colours.backgroundDark} 1%,${colours.background} 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${colours.backgroundDark}', endColorstr='${colours.background}',GradientType=0 );
+  background: ${colour.backgroundDark};
+  background: -moz-linear-gradient(top, ${colour.backgroundDark} 1%, ${colour.background} 100%);
+  background: -webkit-gradient(linear, left top, left bottom, color-stop(1%,${colour.backgroundDark}), color-stop(100%,${colour.background}));
+  background: -webkit-linear-gradient(top, ${colour.backgroundDark} 1%,${colour.background} 100%);
+  background: -o-linear-gradient(top, ${colour.backgroundDark} 1%,${colour.background} 100%);
+  background: -ms-linear-gradient(top, ${colour.backgroundDark} 1%,${colour.background} 100%);
+  background: linear-gradient(top, ${colour.backgroundDark} 1%,${colour.background} 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='${colour.backgroundDark}', endColorstr='${colour.background}',GradientType=0 );
   box-shadow: 
-      0px 0px 0px 1px ${colours.background}, 
-      1px 0px 0px 0px ${colours.backgroundDark} inset, 
+      0px 0px 0px 1px ${colour.background}, 
+      1px 0px 0px 0px ${colour.backgroundDark} inset, 
       0px 2px 2px rgba(0,0,0,0.1);
   `
 const Description = styled.p`
@@ -50,7 +51,7 @@ const Description = styled.p`
   -webkit-box-orient: vertical;
   cursor: pointer;
   font-style: italic;
-  color: ${colours.primary};
+  color: ${colour.secondary};
   line-height: 23px;
   font-size: ${fonts.main.size.small};
   transition: all 0.5s;
--- src/components/common/styles/index.js
+++ src/components/common/styles/index.js
@@ -11,13 +11,24 @@ const fonts = {
     },
 }
 const colours = {
-    primary: '#ffd01e',
-    secondary: '#96c540',
-    tertiary: '#32ac6a',
-    quaternary: '#008d80',
-    background: '#006a7a',
-    backgroundDark: '#2f4858',
-    primaryShadow: 'rgba(255, 208, 30, 0.5)'
+    base: {
+        primary: '#6f6f6f',
+        secondary: '#9c9c9c',
+        tertiary: '#eeeeee',
+        quaternary: '#dddddd',
+        background: '#ffffff',
+        backgroundDark: '#eaeaea',
+        primaryShadow: 'rgba(90, 90, 90, 0.5)'
+    },
+    ocean: {
+        primary: '#ffd01e',
+        secondary: '#96c540',
+        tertiary: '#32ac6a',
+        quaternary: '#008d80',
+        background: '#006a7a',
+        backgroundDark: '#2f4858',
+        primaryShadow: 'rgba(255, 208, 30, 0.5)'
+    }
 }
 
 
