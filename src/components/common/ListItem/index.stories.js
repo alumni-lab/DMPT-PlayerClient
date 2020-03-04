6d59ccbe8909ee677864f6475f35b8241a5d1f8a
Jerry Chuang <48072980+JerChuang@users.noreply.github.com>
2020-03-03 18:35:53 -0800

    Update variable nameing convention for dummy data

--- src/components/common/ListItem/index.stories.js
+++ src/components/common/ListItem/index.stories.js
@@ -5,13 +5,13 @@ import ListItem from './index';
 const shortItem = {
   id: 1,
   itemType: 'Note',
-  title: 'Some Item',
+  name: 'Some Item',
   description: 'This is a short description'
 }
 const longItem = {
   id: 2,
   itemType: 'Note',
-  title: 'Some Long Item',
+  name: 'Some Long Item',
   description: 'Lorem ipsum\n dolor sit amet, consectetur adipiscing elit. Aliquam consectetur venenatis blandit. Praesent vehicula, libero non pretium vulputate, lacus arcu facilisis lectus, sed feugiat tellus nulla eu dolor. Nulla porta bibendum lectus quis euismod. Aliquam volutpat ultricies porttitor. Cras risus nisi, accumsan vel cursus ut, sollicitudin vitae dolor. Fusce scelerisque eleifend lectus in bibendum. Suspendisse lacinia egestas felis a volutpat.'
 }
 const modalTemp = (item, options) => {
